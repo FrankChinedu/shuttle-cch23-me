69a38be449fa890b9cd2e1b69dd4c89af7188336
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-11-28 20:51:51 +0100

    add error url

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{get, web::ServiceConfig, HttpResponse};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
+#[get("/-1/error")]
+async fn handle_error() -> HttpResponse {
+    HttpResponse::InternalServerError().into()
+}
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.service(hello_world).service(handle_error);
     };
 
     Ok(config.into())
