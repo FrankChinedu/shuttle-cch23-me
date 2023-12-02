21335125ef13d78bdab07a2944cebe69a4a7cf80
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-12-02 22:56:00 +0100

    day one code hunt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{get, web::ServiceConfig, HttpResponse};
+use actix_web::{
+    get,
+    web::{Path, ServiceConfig},
+    HttpRequest, HttpResponse,
+};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
@@ -10,10 +14,30 @@ async fn handle_error() -> HttpResponse {
     HttpResponse::InternalServerError().into()
 }
 
+#[derive(Debug, serde::Deserialize)]
+struct XorPath {
+    nums: Vec<u32>,
+}
+
+#[get("/1/{nums:.*}")]
+async fn xor_operation(req: HttpRequest) -> String {
+    let res = req
+        .match_info()
+        .query("nums")
+        .split('/')
+        .map(|x| x.parse::<i32>().expect("msg"))
+        .fold(0, |acc, x| acc ^ x)
+        .pow(3);
+
+    format!("{res:?}")
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world).service(handle_error);
+        cfg.service(hello_world)
+            .service(handle_error)
+            .service(xor_operation);
     };
 
     Ok(config.into())
